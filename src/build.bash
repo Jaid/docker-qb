9d3217e8ac7ba6a2fdf07aaa4dde2bbb380fa650
Jaid <jaid.jsx@gmail.com>
2022-12-24 23:11:19 +0100

    Trying things
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/build.bash
+++ src/build.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# repoSha=303df78232765713bbb459ffb0d50a930e4a7304
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes update
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes install curl
 
-# DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes update
-# DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes upgrade
-# DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes install curl
-# DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes install build-essential libssl-dev libpcre3-dev libcurl4-openssl-dev
-# curl --location --fail --show-error https://github.com/10gic/vanitygen-plusplus/archive/$repoSha.tar.gz --output vanitygen-plusplus.tar.gz
-# tar -x -z -f vanitygen-plusplus.tar.gz
-# mv vanitygen-plusplus-$repoSha vanitygen-plusplus
-# make --directory vanitygen-plusplus
+repoSha=900dd69dcb790a008fdd33186b0e383893fc4402
+curl --location --fail --show-error https://github.com/userdocs/qbittorrent-nox-static/archive/$repoSha.tar.gz --output qbStatic.tar.gz
+tar -x -z -f qbStatic.tar.gz
+mv qbittorrent-nox-static-$repoSha qbStatic
+cd qbStatic
+qbt_optimise_strip=yes qbt_libtorrent_version=2.0 qbt_qt_version=6.3.1 bash -o xtrace qbittorrent-nox-static.sh all
