a7755ada2ec822ff4e8b71315212349e57a03e72
Jaid <jaid.jsx@gmail.com>
2022-12-31 23:10:58 +0100

    Fixed repo downloads
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/build2.bash
+++ src/build2.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-repo=userdocs/qbittorrent-nox-static
+repoOwner=userdocs
+repoName=qbittorrent-nox-static
 repoSha=900dd69dcb790a008fdd33186b0e383893fc4402
 tarFileName=qbStatic.tar.gz
 
 cd
-curl --location --retry 3 --fail --silent --show-error https://github.com/$repo/archive/$repoSha.tar.gz --output $tarFileName
+curl --location --retry 3 --fail --silent --show-error https://github.com/$repoOwner/$repoName/archive/$repoSha.tar.gz --output $tarFileName
 tar -x -z -f $tarFileName
 rm $tarFileName
-mv qbittorrent-nox-static-$repoSha qbStatic
+mv $repoName-$repoSha qbStatic
--- src/build4.bash
+++ src/build4.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-repo=dracula/qbittorrent
+repoOwner=dracula
+repoName=qbittorrent
 repoSha=a9d64acd1faf2d23f46a98b20eba6640805a0f62
 tarFileName=dracula.tar.gz
 
 cd
-curl --location --retry 3 --fail --silent --show-error https://github.com/$repo/archive/$repoSha.tar.gz --output $tarFileName
+curl --location --retry 3 --fail --silent --show-error https://github.com/$repoOwner/$repoName/archive/$repoSha.tar.gz --output $tarFileName
 tar -x -z -f $tarFileName
 rm $tarFileName
-mv qbittorrent-nox-static-$repoSha qbDracula
+mv $repoName-$repoSha qbDracula
