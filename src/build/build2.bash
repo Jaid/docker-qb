3423de956583c72fbe2f16d136ee274b716045cf
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-25 19:00:21 +0200

    Fixed libtorrentVersion
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/build/build2.bash
+++ src/build/build2.bash
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
+if [[ -z "$libtorrentVersion" ]]; then
+  echo "libtorrentVersion environment variable must be set"
+  exit 1
+fi
+
 export DEBIAN_FRONTEND=noninteractive
-export qbt_libtorrent_version=2.0
+export qbt_libtorrent_version="${libtorrentVersion%.*}"
 export qbt_qt_version=6
 export qbt_build_tool=cmake
 export buildDirectory=/root/qbBuild
 
-time bash ~/qbStatic/qbittorrent-nox-static.sh all --libtorrent-tag v2.0.8 --qbittorrent-tag master --icu --cmake --strip --build-directory "$buildDirectory"
+time bash ~/qbStatic/qbittorrent-nox-static.sh all --libtorrent-tag "v$libtorrentVersion" --qbittorrent-tag master --icu --cmake --strip --build-directory "$buildDirectory"
 du -sb "$buildDirectory/bin/qbittorrent-nox"
