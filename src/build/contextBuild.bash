41f4d3ce56167a1a1f96d69ee81e0c1c1f0a0180
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-26 08:07:18 +0200

    Reverted debugging changes
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/build/contextBuild.bash
+++ src/build/contextBuild.bash
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-env
-# libtorrentVersion=${libtorrentVersion:-2.0.9}
-if [[ -z $abc ]]; then
-  printf >&2 'Variable $abc not existing\n'
+if [[ -z $libtorrentVersion ]]; then
+  printf >&2 'Variable $libtorrentVersion not existing\n'
   exit 1
 fi
-printf 'abc = %s\n' "$abc"
-
 export DEBIAN_FRONTEND=noninteractive
 export qbt_libtorrent_version="${libtorrentVersion%.*}"
 export qbt_qt_version=6
